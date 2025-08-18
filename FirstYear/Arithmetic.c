c19945f5b5f2d8e6a5e845d45ca4c8a401852ed5
Vignesh P <vigneshp.1818@gmail.com>
2025-08-18 22:48:56 +0530

    Committed Arithmetic.c

--- FirstYear/Arithmetic.c
+++ FirstYear/Arithmetic.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int a=10,b=5,c;
-    c=-a;//This is Unary minus Operator
+    c=-a;  //This is Unary minus Operator
     printf("Value of c is %d\n",c);
     printf("(a+b)=%d\n",a+b);
     printf("(a-b)=%d\n",a-b);
